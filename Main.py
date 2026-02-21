c7548369575ae7076616e0599f63a3693de39e2a
Utsav0706 <utsavjivani.07@gmail.com>
2026-02-21 15:02:22 +0000

    Put comment for coder

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
+# import all modules to the main class
 import random
 import art, words
 
 lives = 6
 
+# display logo to look good
 print(art.logo)
 
 chosenWord = random.choice(words.wordlist)
@@ -20,9 +22,12 @@ gameOver = False
 correctLetters = []
 
 while not gameOver:
+    # telling user that howmany lives they have left
     print(f"**************** {lives}/6 Lives Left ****************")
 
     guess = input("Guess a letter: ").lower()
+
+    # If the user has entered a word that they already guessed then tell them about it...
     if guess in correctLetters:
         print(f"You already guessed {guess}")
 
@@ -38,6 +43,7 @@ while not gameOver:
             display += "_"
     print("Word to guess " + display)
 
+    # if the letter is not in the chosen word then let them know that they are loosing a chance
     if guess not in chosenWord:
         lives -= 1
         print(f"You guessed the word {guess}, That's not in the word, You lose.... :(")
@@ -46,6 +52,7 @@ while not gameOver:
             gameOver = True
             print(f"*****************It was {chosenWord} :( You Lose! *****************")
 
+    # if they guessed all right that huurrrreeeeeehhhhhhhhh, they win
     if "_" not in display:
         gameOver = True
         print("*************** You Win! ***************")
